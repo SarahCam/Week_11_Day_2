08b65debdbe016568a947be9dfeb4447bc44a8d5
Sarah Campbell <scampbell.home@btinternet.com>
2018-04-03 22:59:53 +0100

    TDD test for 'should be able to remove all dinosaurs of a particular type'

--- park.js
+++ park.js
@@ -6,4 +6,14 @@ Park.prototype.addDino = function (dino) {
   this.enclosure.push(dino);
 };
 
+Park.prototype.removeDinosOfType = function (type) {
+  let updatedEnclosure = [];
+  for (let dino of this.enclosure) {
+    if (dino.type !== type) {
+      updatedEnclosure.push(dino);
+    };
+  };
+  this.enclosure = updatedEnclosure;
+};
+
 module.exports = Park;
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -17,6 +17,9 @@ describe('Park', function(){
   beforeEach(function(){
     park = new Park();
     dino1 = new Dino('Velociraptor', 3);
+    dino2 = new Dino('Tyrannosaurus', 1);
+    dino3 = new Dino('Velociraptor', 3);
+    dino4 = new Dino('Triceratops', 2);
   });
 
   it('enclosure should start empty', function(){
@@ -26,6 +29,19 @@ describe('Park', function(){
   it('should be able to add dinosaur', function(){
     park.addDino(dino1);
     assert.strictEqual(park.enclosure.length, 1);
+    assert.deepStrictEqual(park.enclosure, [dino1]);
+  });
+
+  it('should be able to remove all dinosaurs of a particular type', function(){
+    park.addDino(dino1);
+    park.addDino(dino2);
+    park.addDino(dino3);
+    park.addDino(dino4);
+    assert.strictEqual(park.enclosure.length, 4);
+    assert.deepStrictEqual(park.enclosure, [dino1, dino2, dino3, dino4]);
+    park.removeDinosOfType('Velociraptor');
+    assert.strictEqual(park.enclosure.length, 2);
+    assert.deepStrictEqual(park.enclosure, [dino2, dino4]);
   });
 
 });
